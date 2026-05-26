7359a355028a69c195ef1dc0c78cf41b16747f84
MohammadAbrarAlam <abrarimam78602gmail.com>
2026-05-27 00:22:37 +0530

    Fix Render deployment port issue

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 from flask import Flask, request, jsonify
 from crews import medical_awareness_crew
-
+import os
 app = Flask(__name__)
 
 
@@ -77,5 +77,8 @@ def analyze():
 # ==========================================
 # Run Server
 # ==========================================
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
